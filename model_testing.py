a0e68d42d2d376048cf1fa189c79b1bef4a2ee73
Mikhail Markin <99mmarkin@gmail.com>
2023-03-01 03:39:29 +0500

    Added files

---
+++ model_testing.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from sklearn.metrics import accuracy_score
+import joblib
+
+def predict():
+
+    df_test = pd.read_csv('test/test_data.csv', sep='~')
+
+    model = joblib.load('model.pkl')
+
+    pred = model.predict(df_test.drop(columns={'target'}))
+
+    print(accuracy_score(pred, df_test.target))
+
+if __name__ == '__main__':
+    predict()
